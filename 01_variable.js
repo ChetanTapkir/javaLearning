74ef57219267a1dc8e8408fd11f45d14711c8f97
ChetanTapkir <tapkirchetan3@gmail.com>
2022-12-08 17:41:28 +0530

    FirstjsProgrammeCode

---
+++ 01_variable.js
@@ -0,0 +1,17 @@
+var myname;
+myname="Chetan";
+console.log(myname);
+
+var myFndName="Sachin Tendulkar";
+console.log(myFndName);
+myFndName="MS Dhoni";
+console.log(myFndName);
+var MyFullName;
+console.log(MyFullName);
+MyFullName="Chetan Shivaji Tapkir";
+console.log(MyFullName);
+var MyLove="Love Java Scirpt";
+console.log(MyLove);
+
+var firstName="Chetan";var lastname="Tapkir";var age="30";var adress=" Dange Chawk Thergaon Pune"; var pincode="411033"; var gender="male";var marriatal_status="yes";
+console.log(firstName,lastname,age,adress,pincode,gender,marriatal_status);
