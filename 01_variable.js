b33bbabe86931817446d1cf0decfc03a18e869aa
ChetanTapkir <tapkirchetan3@gmail.com>
2022-12-10 06:50:46 +0530

    jsassm1

--- 01_variable.js
+++ 01_variable.js
@@ -15,3 +15,4 @@ console.log(MyLove);
 
 var firstName="Chetan";var lastname="Tapkir";var age="30";var adress=" Dange Chawk Thergaon Pune"; var pincode="411033"; var gender="male";var marriatal_status="yes";
 console.log(firstName,lastname,age,adress,pincode,gender,marriatal_status);
+
---
+++ swapver.js
@@ -0,0 +1,46 @@
+
+var Ass="*Assignment Swap Two Values ";
+console.log(Ass);
+
+var myNumber =100;
+var yourNumber=300;
+
+var comm="============================Befor swap===================================";
+console.log(comm);
+console.log(myNumber,yourNumber);
+
+var comm1="===========================after swap===================================";
+
+var temp=myNumber;
+myNumber=yourNumber;
+yourNumber=temp;
+ Ass="*Assignment Swap Two Vaariables";
+
+// console.log(Ass);
+
+console.log(comm1);
+console.log(myNumber,yourNumber);
+
+console.log(comm);
+
+var a="Sweety";
+var b="Cutie" 
+console.log(a,b);
+temp=a;
+a=b;
+b=temp;
+console.log(comm1);
+
+console.log(a,b);
+var num1=1000;
+var num2=2000;
+var num3=3000;
+console.log(comm);
+console.log(num1,num2,num3);
+// before swap
+var temp=num1;
+num1=num2;
+num2=num3;
+num3=temp;
+console.log(comm1);
+console.log(num1,num2,num3);
