91d357b993cf4553648521a0686d134848441649
ChetanTapkir <tapkirchetan3@gmail.com>
2022-12-11 16:50:45 +0530

    Update newjsassm1.js

--- newjsassm1.js
+++ newjsassm1.js
@@ -41,3 +41,9 @@ return temp;
  bankDetails("City Bank",3456782345,"Pune",431202);
  bankDetails("Axis Bank",7856782345,"Mumbai",441202);
  bankDetails("HDFC Bank",8956782345,"pune",631202,"open");
+ var foo = 1;
+ function myfun(){
+    console.log(foo);
+    var foo = 2;
+ }
+ myfun();
