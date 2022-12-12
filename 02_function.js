48ad6528d5fe92784b203e42717f4c4aca40c54a
ChetanTapkir <tapkirchetan3@gmail.com>
2022-12-12 07:03:42 +0530

    submit

---
+++ 02_function.js
@@ -0,0 +1,12 @@
+function swapValues(val1,val2){
+console.log("before swap",val1,val2);
+var temp=val1;
+val1=val2;
+val2=temp;
+console.log("after swap",val1,val2);
+
+}
+swapValues(100,200);
+swapValues("chetan","tapkir");
+swapValues(899,988);
+swapValues("you","&me")
---
+++ assgnmnt.js
@@ -0,0 +1,5 @@
+function f1()
+{
+    console.log("this is function with no orgument and no return type");
+}
+f1();
