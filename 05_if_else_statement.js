b3be5dbdebd5b8e1490726b3ae8855514be0c342
ChetanTapkir <tapkirchetan3@gmail.com>
2022-12-19 08:31:24 +0530

    Create 05_if_else_statement.js

---
+++ 05_if_else_statement.js
@@ -0,0 +1,10 @@
+console.log(`Broom Broom`);
+var isRoadBlocked = true;
+if (isRoadBlocked)
+{console.log(`Traveling thru express way`);
+console.log(`Travel will be of 2`);
+}
+else{
+    console.log(`Traveling thru lonavala old highway`);
+    console.log(`Travels will be of 4 HRS`);
+}
