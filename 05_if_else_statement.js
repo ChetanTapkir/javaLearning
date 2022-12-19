ed6bb5cd688d27aa4fc48f008e10465d4264f166
ChetanTapkir <tapkirchetan3@gmail.com>
2022-12-19 08:50:13 +0530

    Update 05_if_else_statement.js

--- 05_if_else_statement.js
+++ 05_if_else_statement.js
@@ -8,3 +8,16 @@ else{
     console.log(`Traveling thru lonavala old highway`);
     console.log(`Travels will be of 4 HRS`);
 }
+function isEvenOrOdd(num)
+{
+if (typeof num=="number")
+{
+    if(num%2==0){console.log(`Even No`);}
+    else{console.log(`odd No`);}
+}
+else{console.log(`Invalid No`);}
+}
+
+isEvenOrOdd(20);
+isEvenOrOdd(11);
+isEvenOrOdd("30");
