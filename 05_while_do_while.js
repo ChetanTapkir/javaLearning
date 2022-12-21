6f34e443a36edee0e4b05f4a3af6d3899d1e1f24
ChetanTapkir <tapkirchetan3@gmail.com>
2022-12-21 22:30:53 +0530

    commit on 21 dec

---
+++ 05_while_do_while.js
@@ -0,0 +1,48 @@
+i=5
+while(i<=15){
+console.log(i);
+i++
+}
+
+i=50
+while(i>=40){
+console.log(i);
+i--
+}
+i=0
+while(i<=15){
+    if(i%2!=0){
+console.log(i);}
+i++
+}
+
+i=1;
+while(i<=10){
+    if(i%2==0){
+console.log(i);}
+i++
+}
+
+var a=0;
+do{
+a=a+5
+console.log(a);
+
+}while(a<=50)
+
+var a=0;
+do{
+a=a+10
+console.log(a);
+
+
+}while(a<=100)
+
+
+var a=100;
+do{
+a=a-10
+console.log(a);
+
+
+}while(a>=0)
