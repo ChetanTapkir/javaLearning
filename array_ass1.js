653bb36c62cb94fb731d835b2d6b0e5172764e4f
ChetanTapkir <tapkirchetan3@gmail.com>
2022-12-24 20:58:17 +0530

    comment today
    comment today

--- array_ass1.js
+++ array_ass1.js
@@ -15,8 +15,11 @@ for (let  index= 0;  index< arrayofNo.length; index++) {
     console.log(`1st occurance of lelment 10 is :${index}`);}}
 }
 
-for (let  index= 0;  index< arrayofNo.length; index++) {
+for (let  index= 0;  index< arrayofNo.length; index++) 
+{
     var element = arrayofNo[index];
- {if(element==6){
-    console.log(`1st occurance of lelment 10 is :${index}`);}}
+ 
+    if(element==6){console.log(`1st occurance of lelment 6 is :${index}`);}
+    // console.log(`2nd occurance of lelment 10 is :${index}`);
+
 }
--- cunt_and_print_owels.js
+++ cunt_and_print_owels.js
@@ -2,16 +2,17 @@
 var as="I am very good IT Developer";
 var count =0;
     for (let index = 0; index < as.length; index++)
-     {
-        
-     if(as.charAt(index)==`a`||as.charAt(index)==`e`||as.charAt(index)==`i`||as.charAt(index)==`o`||as.charAt(index)==`u`||
-     as.charAt(index)==`A`||as.charAt(index)==`E`||as.charAt(index)==`I`||as.charAt(index)==`O`||as.charAt(index)==`U`) 
-     {let char = as.charAt(index);
-        console.log(char);}
-     count++;
-     
-     } console.log(count);
-console.log("=============cube of 5=====================");
+     {char=as.charAt(index)
+      var charcopy=char.toLocaleLowerCase();  
+     if(charcopy==`a`||charcopy==`e`||charcopy==`i`||charcopy==`o`||charcopy==`u`) 
+     {let prit = as.charAt(index);
+      console.log(`the owels in available string are ${prit}`);
+     }
+      count=count++;
+          } console.log(count);
+          
+
+     console.log("=============cube of 5=====================");
 var num=5;     
 for (let i = 0; i<1; i++) 
      {
