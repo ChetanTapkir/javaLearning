620ea20e0716d552ae6c4e2c7282502288207bee
ChetanTapkir <tapkirchetan3@gmail.com>
2022-12-26 07:15:55 +0530

    cm and pu
    cm and push

--- 07_learn_array.js
+++ 07_learn_array.js
@@ -1,6 +1,14 @@
 // for (let index = 0; index < array.length; index++) {
 //     const element = array[index];
 //     }
+// let arrayOfNumbers = [5, 10, 4, 6, 7, 10, 6]; // Empty Array
+// console.log(arrayOfNumbers);
+
+// console.log(`===== Total number of elements available in array ====`);
+// let totalElements = arrayOfNumbers.length;
+// console.log(`Total Elements in arrayOfNumbers --> ${totalElements}`);
+
+
 let arrayOfNumbers = [5, 10, 4, 6, 7, 10, 6]; // Empty Array
 console.log(arrayOfNumbers);
 
@@ -9,12 +17,7 @@ let totalElements = arrayOfNumbers.length;
 console.log(`Total Elements in arrayOfNumbers --> ${totalElements}`);
 
 
-let arrayOfNumbers = [5, 10, 4, 6, 7, 10, 6]; // Empty Array
-console.log(arrayOfNumbers);
 
-console.log(`===== Total number of elements available in array ====`);
-let totalElements = arrayOfNumbers.length;
-console.log(`Total Elements in arrayOfNumbers --> ${totalElements}`);
 
 let typeOfArray =typeof arrayOfNumbers;
 console.log(`Type of array - arrayOfNumbers --> ${typeOfArray}`);
@@ -42,7 +45,7 @@ console.log(`index of element 7 is ${indexof7}`);
 
 console.log("==============traversing array using for loop ============");
 for (let index = 0; index < arrayOfNumbers.length; index++) {
-    const element = array[index];
+    const element = arrayOfNumbers[index];
     console.log(element);
 }
 
@@ -52,3 +55,14 @@ for (let index = 0; index < arrayOfNumbers.length; index++) {
  if{   const element = array[index];
     console.log(element);}
 }
+
+
+
+let salary =[10000,20000,30000,40000,50000];
+var size =salary.length;
+var sumofsal =0;
+for (let index = 0; index < size; index++) {
+    const element = salary[index];
+    sumofsal=sumofsal+element;
+    console.log(element);
+}console.log(sumofsal);
