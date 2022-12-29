f6d5e16238ecf27c66dfc876f2df9cbb25addaef
ChetanTapkir <tapkirchetan3@gmail.com>
2022-12-29 22:04:59 +0530

    Create object_Assignment.js

---
+++ object_Assignment.js
@@ -0,0 +1,20 @@
+const sbiBank  ={
+bankName : "SBI Bank",
+location : "CHINCHWAD",
+accountNo : 50100043900,
+IFSC : "SBIN000437",
+interestRate : 8.75,
+     showDetails : function(){console.log(sbiBank);}
+}
+sbiBank.showDetails();     
+
+
+     const axisBank  ={
+                    bankName : "Axis Bank",
+                    location : "Swarget",
+                    accountNo : 100140045275,
+                    IFSC : "UTIB000475",
+                    interestRate : 8.6,
+                    showDetails : function(){console.log(sbiBank);}
+                }
+            axisBank.showDetails();                                   
